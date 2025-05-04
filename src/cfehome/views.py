5032cd8af050d846c4a2ee5e9d50767e7bfad7e7
MejriRami <ramimejri76@gmail.com>
2025-05-04 19:30:49 +0100

    prepare for railway3

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -8,11 +8,10 @@ def home_page_view(request, *args ,**kwargs):
     page_qs=PageVisit.objects.filter(path=request.path)
     my_title = "My Page"
     html_template = "home.html"
-
     my_context = {
         "page_title": my_title,
         "page_visit_count":page_qs.count(),
-        "percent":(page_qs.count()* 100.0)/qs.count(),
+        #"percent":(page_qs.count()* 100.0)/qs.count(),
         "total_visit_count":qs.count(),
     }
     path= request.path
