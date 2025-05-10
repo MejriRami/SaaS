c9141253d754435a407c1b111e701093aa6e593d
MejriRami <ramimejri76@gmail.com>
2025-05-10 18:49:00 +0100

    stripe complet

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,51 +1,36 @@
-from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-# Create your views here.
+
 from django.contrib.auth import get_user_model
-User = get_user_model() 
-
-
-
+User = get_user_model()
 
 @login_required
-def  profile_list_view(request, *args, **kwargs):
-    context={
-        "object_list":User.objects.filter(is_active=True)
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
     }
+    return render(request, "profiles/list.html", context)
 
-    return render(request,"profiles/list.html",context)
-
-
-
-
-
-
-
-
-
-
-
-@login_required 
-def profile_view(request,username=None ,  *args, **kwargs):
-    print('user.has_perm("auth.view_user")', user.has_perm ("auth.view_user"))
-    print('user.has_perm("visits.view_pagevisit")', user. has_perm("visits.view_pagevisit"))
-    user =  request.user
-    profile_user_obj=get_object_or_404(User, username=username)
-    is_me=profile_user_obj==username
-    return HttpResponse(f"hello there {username} - {profile_user_obj.id}- {user.id}-{is_me}")  
 
 @login_required
 def profile_detail_view(request, username=None, *args, **kwargs):
-    user =  request.user
-    user_groups=user.groups.all()
-    if user_groups.filter(name__icontains="basic").exists():
-        return HttpResponse("congrats")
-    profile_user_obj=get_object_or_404(User, username=username)
-    is_me=profile_user_obj==username
-    context={
-        "object":profile_user_obj,
-        "instance":profile_user_obj,
-        "owner":is_me,
+    user = request.user
+    print(
+        user.has_perm("subscriptions.basic"),
+        user.has_perm("subscriptions.basic_ai"),
+        user.has_perm("subscriptions.pro"),
+        user.has_perm("subscriptions.advanced"),    
+    )
+    # user_groups = user.groups.all()
+    # print("user_groups", user_groups)
+    # if user_groups.filter(name__icontains='basic').exists():
+    #     return HttpResponse("Congrats")
+    profile_user_obj = get_object_or_404(User, username=username)
+    is_me = profile_user_obj == user
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
     }
-    return render(request,"profiles/detail.html",context),
+    return render(request, "profiles/detail.html", context)
